98b0c73b544b9f9f1cf1c1c72256277be20d3951
jonathanvonoppen <vonoppenj@gmail.com>
2026-03-10 17:56:43 +0100

    account for GLORIA-specific format

--- scripts/data_cleaning/data_screening.R
+++ scripts/data_cleaning/data_screening.R
@@ -48,6 +48,7 @@ gloria_data_dir <- file.path(data_dir, "external", "GLORIA")
 
 # plot metadata
 gloria_plot_metadata <- readr::read_delim(file.path(gloria_data_dir, "fld_plot.txt"))
+  #> date_survey currently in format dd.mm.yyyy 00:00:00
 
     # n unique regions
     gloria_plot_metadata %>% pull(region) %>% unique() %>% length()  #> 27
@@ -56,6 +57,7 @@ gloria_plot_metadata <- readr::read_delim(file.path(gloria_data_dir, "fld_plot.t
 
 # plot cover data
 gloria_plot_cover <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_protocols.txt"))
+  #> cover currently character with , delimiter
 
 # additional plot-level data
 gloria_plot_data_add <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_additional_infos.txt"))
@@ -63,6 +65,7 @@ gloria_plot_data_add <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_a
 # species data
 gloria_species <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_species.txt"))  
   #> species coded numerically; species_code corresponds to column $id in gloria_species_reference (file *lut_species.txt*)
+  #> cover currently character with , delimiter
 
 # species reference list
 gloria_species_reference <- readr::read_delim(file.path(gloria_data_dir, "lut_species.txt")
@@ -74,3 +77,16 @@ gloria_plot_topologies <- readr::read_delim(file.path(gloria_data_dir, "fld_plot
 # summit-level data
 gloria_summit_data <- readr::read_delim(file.path(gloria_data_dir, "fld_summit.txt")
                                         , delim = ";")
+  #> summit names with special characters messed up (due to default UTF-8 encoding during read)
+  #> latitude/longitude messed up (due to default . decimal mark and , grouping mark)
+
+
+# specific read function to account for special characters, decimal , and . grouping mark in GLORIA files:
+read_gloria_file <- function(file) {
+  readr::read_delim(file = file,
+                    , delim = ";"
+                    , locale = locale(decimal_mark = ",",
+                                      grouping_mark = ".",
+                                      encoding = "latin1"
+                    ))
+}
