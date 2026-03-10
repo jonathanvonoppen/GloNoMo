7b59fb036ee0aa1145f051025940b6878bcd3f58
jonathanvonoppen <vonoppenj@gmail.com>
2026-03-10 17:16:30 +0100

    include & screen GLORIA data

--- scripts/data_cleaning/data_screening.R
+++ scripts/data_cleaning/data_screening.R
@@ -20,25 +20,57 @@ miren_data_dir <- file.path(data_dir, "external", "MIREN")
 
 # [1] Load data ----
 
-## >> roads ----
+## MIREN ----
+
+### >> roads ----
 miren_roads <- readr::read_csv(file.path(miren_data_dir, "MIREN_road_survey_(2007-2023)_zen_v3_002507.csv"))
 
 # n unique regions
-miren_roads %>% pull(Region) %>% unique() %>% length()
-#> 26
+miren_roads %>% pull(Region) %>% unique() %>% length()  #> 26
 
 # n unique roads
-miren_roads %>% pull(Road) %>% unique() %>% length()
-#> 75
+miren_roads %>% pull(Road) %>% unique() %>% length()  #> 75
 
-## >> trails ----
+### >> trails ----
 miren_trails <- readxl::read_xlsx(file.path(miren_data_dir, "MIREN_trail_data_final.xlsx")
                                   , na = c("", "NA"))
 
 # n unique regions
-miren_trails %>% pull(Region) %>% unique() %>% length()
-#> 10
+miren_trails %>% pull(Region) %>% unique() %>% length()  #> 10
 
 # n unique roads
-miren_trails %>% pull(Trail) %>% unique() %>% length()
-#> 53
+miren_trails %>% pull(Trail) %>% unique() %>% length()  #> 53
+
+
+## GLORIA ----
+
+gloria_data_dir <- file.path(data_dir, "external", "GLORIA")
+
+# plot metadata
+gloria_plot_metadata <- readr::read_delim(file.path(gloria_data_dir, "fld_plot.txt"))
+
+    # n unique regions
+    gloria_plot_metadata %>% pull(region) %>% unique() %>% length()  #> 27
+    # n unique plots
+    gloria_plot_metadata %>% tidyr::unite(col = "plot_unique", region, summit, plot) %>% pull(plot_unique) %>% unique() %>% length()  #> 2600
+
+# plot cover data
+gloria_plot_cover <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_protocols.txt"))
+
+# additional plot-level data
+gloria_plot_data_add <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_additional_infos.txt"))
+
+# species data
+gloria_species <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_species.txt"))  
+  #> species coded numerically; species_code corresponds to column $id in gloria_species_reference (file *lut_species.txt*)
+
+# species reference list
+gloria_species_reference <- readr::read_delim(file.path(gloria_data_dir, "lut_species.txt")
+                                              , delim = ";")
+
+# plot-level topologies
+gloria_plot_topologies <- readr::read_delim(file.path(gloria_data_dir, "fld_plot_topologies.txt"))
+
+# summit-level data
+gloria_summit_data <- readr::read_delim(file.path(gloria_data_dir, "fld_summit.txt")
+                                        , delim = ";")
