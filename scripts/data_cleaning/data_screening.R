918f77238bda9dd3ae883b311765f77f52d8018a
jonathanvonoppen <vonoppenj@gmail.com>
2026-03-10 17:19:39 +0100

    adjust variables & script layout

--- scripts/data_cleaning/data_screening.R
+++ scripts/data_cleaning/data_screening.R
@@ -23,23 +23,23 @@ miren_data_dir <- file.path(data_dir, "external", "MIREN")
 ## MIREN ----
 
 ### >> roads ----
-miren_roads <- readr::read_csv(file.path(miren_data_dir, "MIREN_road_survey_(2007-2023)_zen_v3_002507.csv"))
+miren_road_species <- readr::read_csv(file.path(miren_data_dir, "MIREN_road_survey_(2007-2023)_zen_v3_002507.csv"))
 
-# n unique regions
-miren_roads %>% pull(Region) %>% unique() %>% length()  #> 26
-
-# n unique roads
-miren_roads %>% pull(Road) %>% unique() %>% length()  #> 75
+    # n unique regions
+    miren_road_species %>% pull(Region) %>% unique() %>% length()  #> 26
+    
+    # n unique roads
+    miren_road_species %>% pull(Road) %>% unique() %>% length()  #> 75
 
 ### >> trails ----
-miren_trails <- readxl::read_xlsx(file.path(miren_data_dir, "MIREN_trail_data_final.xlsx")
+miren_trail_plot_metadata <- readxl::read_xlsx(file.path(miren_data_dir, "MIREN_trail_data_final.xlsx")
                                   , na = c("", "NA"))
 
-# n unique regions
-miren_trails %>% pull(Region) %>% unique() %>% length()  #> 10
-
-# n unique roads
-miren_trails %>% pull(Trail) %>% unique() %>% length()  #> 53
+    # n unique regions
+    miren_trail_plot_metadata %>% pull(Region) %>% unique() %>% length()  #> 10
+    
+    # n unique roads
+    miren_trail_plot_metadata %>% pull(Trail) %>% unique() %>% length()  #> 53
 
 
 ## GLORIA ----
